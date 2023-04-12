00e1bf08bd7fe0d841584f2ccaab498b9f58f83b
Gabriel <gamcode98@gmail.com>
2023-04-12 16:50:46 -0300

    add: fetch to events from api

--- src/screens/HomeScreen/index.jsx
+++ src/screens/HomeScreen/index.jsx
@@ -1,19 +1,43 @@
-import { FlatList, Text, View } from 'react-native'
+import { FlatList, Text, View, ActivityIndicator } from 'react-native'
 import { styles } from './HomeScreen.styles'
-import { data } from '../../mock/data'
 import { NewsCard } from '../../components/NewsCard'
 import { HeaderHome } from '../../components/HeaderHome'
+import { useEffect, useState } from 'react'
+import { getEvents } from '../../services/events'
+import { COLORS } from '../../utils/theme'
 
 export function HomeScreen () {
+  const [events, setEvents] = useState([])
+  const [nextUrl, setNextUrl] = useState(null)
+
+  useEffect(() => {
+    loadEvents()
+  }, [])
+
+  const loadEvents = () => {
+    getEvents(nextUrl)
+      .then((data) => {
+        const { content, nextPage } = data
+        setEvents([...events, ...content])
+        nextPage !== 'null' ? setNextUrl(nextPage) : setNextUrl(null)
+      })
+      .catch(error => {
+        console.log({ error })
+      })
+  }
+
   return (
     <View style={styles.container}>
       <FlatList
-        data={data}
+        data={events}
         renderItem={({ item }) => <NewsCard item={item} />}
-        keyExtractor={item => item.id}
+        keyExtractor={item => item._id}
         ItemSeparatorComponent={<Text> </Text>}
         showsVerticalScrollIndicator={false}
         ListHeaderComponent={() => <HeaderHome />}
+        onEndReached={nextUrl && loadEvents}
+        onEndReachedThreshold={0.1}
+        ListFooterComponent={nextUrl && <ActivityIndicator size='large' color={COLORS.primary} />}
       />
     </View>
   )
